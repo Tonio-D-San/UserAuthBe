d0b8ecb97d8ad40540b2267440ff1baa4025e765
AntonioSansonne <asansonne@cybsec.it>
2025-11-07 20:37:47 +0100

    update notes_dump.sql

--- src/main/java/it/asansonne/authhub/security/WebSecurityConfiguration.java
+++ src/main/java/it/asansonne/authhub/security/WebSecurityConfiguration.java
@@ -2,17 +2,29 @@ package it.asansonne.authhub.security;
 
 import it.asansonne.authhub.exception.handler.AuthorizationAuthenticationHandler;
 import it.asansonne.authhub.security.provider.CustomOauth2UserService;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.stereotype.Component;
 
 /**
  * The type Web security configuration.
@@ -27,43 +39,73 @@ public class WebSecurityConfiguration {
   private final ManageToken manageToken;
 
   @Bean
-  protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  protected SecurityFilterChain filterChain(
+      HttpSecurity http, KeycloakAuthenticationConverter authenticationConverter
+  ) throws Exception {
     log.info("Configuring security filter chain");
     return http
         .addFilterBefore(manageToken, UsernamePasswordAuthenticationFilter.class)
         .cors(Customizer.withDefaults())
+        //TODO keycloak
+        .oauth2ResourceServer(oauth2 ->
+            oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(authenticationConverter))
+        )
+        //Fine TODO
         .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(requests -> requests
-            // Proteggi solo le API
-            .requestMatchers(
-                new AntPathRequestMatcher("/api/v*/**")
-            ).authenticated()
-            .anyRequest()
-            .permitAll()
-        ).formLogin(
-            form -> form
-                .loginPage("/login")
-                .loginProcessingUrl("/login")
-                .defaultSuccessUrl("/api/v1/swagger-ui/index.html", true)
-                .failureUrl("/login?error=true")
-                .permitAll()
-        ).oauth2Login(oauth -> oauth
-            .loginPage("/login")
-            .userInfoEndpoint(userInfo -> userInfo
-                .oidcUserService(customOAuth2UserService)
-            ).defaultSuccessUrl("/api/v1/swagger-ui/index.html", true)
-            .permitAll()
+            .requestMatchers(new AntPathRequestMatcher("/api/v*/**")).authenticated()
+            .anyRequest().permitAll()
         ).logout(logout -> logout
             .logoutSuccessUrl("/")
             .permitAll()
         ).sessionManagement(session -> {
           session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
           session.maximumSessions(1).maxSessionsPreventsLogin(false);
-            }
-        ).exceptionHandling(exceptionHandling -> exceptionHandling
+        }).exceptionHandling(exceptionHandling -> exceptionHandling
             .defaultAuthenticationEntryPointFor(handler, new AntPathRequestMatcher("/api/**"))
             .defaultAccessDeniedHandlerFor(handler, new AntPathRequestMatcher("/api/**"))
         ).build();
+  }
+
+  /**
+   * The Keycloak authentication converter.
+   */
+  @Component
+  @RequiredArgsConstructor
+  protected static class KeycloakAuthenticationConverter
+      implements Converter<Jwt, JwtAuthenticationToken> {
+    private final KeycloakAuthoritiesConverter authoritiesConverter;
+
+    @Override
+    public JwtAuthenticationToken convert(@NonNull Jwt jwt) {
+      return new JwtAuthenticationToken(jwt, authoritiesConverter.convert(jwt),
+          List.of(jwt.getClaimAsString(StandardClaimNames.SUB)).toString()
+      );
+    }
+
+    @Component
+    static class KeycloakAuthoritiesConverter
+        implements Converter<Jwt, List<SimpleGrantedAuthority>> {
+
+      @Override
+      @SuppressWarnings({"unchecked"})
+      public List<SimpleGrantedAuthority> convert(Jwt jwt) {
+        final var realmAccess = (Map<String, Object>) jwt.getClaims()
+            .getOrDefault("resource_access", Map.of());
+        final var client = (Map<String, Object>) realmAccess
+            .getOrDefault("user-auth-be", Map.of());
+        List<String> authorities = new ArrayList<>();
+        authorities.addAll(
+            Stream.of(client.getOrDefault("roles", List.of()))
+                .map(s -> "ROLE_" + s).toList()
+        );
+        authorities.addAll(Arrays
+            .stream(String.valueOf(jwt.getClaims()).split(" "))
+            .map(s -> "SCOPE_" + s).toList()
+        );
+        return authorities.stream().map(SimpleGrantedAuthority::new).toList();
+      }
+    }
   }
 
 }
