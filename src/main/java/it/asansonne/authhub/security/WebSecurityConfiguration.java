ee954c08664beba2d5f59c3fce95eb1370a995fd
Tonio_D_San <peppesansa@libero.it>
2025-11-07 01:24:14 +0100

    half kc settings

--- src/main/java/it/asansonne/authhub/security/WebSecurityConfiguration.java
+++ src/main/java/it/asansonne/authhub/security/WebSecurityConfiguration.java
@@ -2,17 +2,28 @@ package it.asansonne.authhub.security;
 
 import it.asansonne.authhub.exception.handler.AuthorizationAuthenticationHandler;
 import it.asansonne.authhub.security.provider.CustomOauth2UserService;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.stereotype.Component;
 
 /**
  * The type Web security configuration.
@@ -35,19 +46,15 @@ public class WebSecurityConfiguration {
         .cors(Customizer.withDefaults())
         .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(requests -> requests
-            // Proteggi solo le API
-            .requestMatchers(
-                new AntPathRequestMatcher("/api/v*/**")
-            ).authenticated()
-            .anyRequest()
-            .permitAll()
-        ).formLogin(
-            form -> form
-                .loginPage(LOGIN_PROCESSING_URL)
-                .loginProcessingUrl(LOGIN_PROCESSING_URL)
-                .defaultSuccessUrl("/api/v1/swagger-ui/index.html", true)
-                .failureUrl(LOGIN_PROCESSING_URL + "?error=true")
-                .permitAll()
+            .requestMatchers(new AntPathRequestMatcher("/api/v*/**")).authenticated()
+            .anyRequest().permitAll()
+//        ).formLogin(
+//            form -> form
+//                .loginPage(LOGIN_PROCESSING_URL)
+//                .loginProcessingUrl(LOGIN_PROCESSING_URL)
+//                .defaultSuccessUrl("/api/v1/swagger-ui/index.html", true)
+//                .failureUrl(LOGIN_PROCESSING_URL + "?error=true")
+//                .permitAll()
         ).oauth2Login(oauth -> oauth
             .loginPage(LOGIN_PROCESSING_URL)
             .userInfoEndpoint(userInfo -> userInfo
@@ -56,7 +63,8 @@ public class WebSecurityConfiguration {
             .permitAll()
         ).logout(logout -> logout
             .logoutSuccessUrl("/")
-            .permitAll()
+            .invalidateHttpSession(true)
+            .deleteCookies("JSESSIONID")
         ).sessionManagement(session -> {
               session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
               session.maximumSessions(1).maxSessionsPreventsLogin(false);
--- src/main/java/it/asansonne/management/model/Mixture.java
+++ src/main/java/it/asansonne/management/model/Mixture.java
@@ -1,57 +1,57 @@
-package it.asansonne.management.model;
-
-import it.asansonne.authhub.model.Models;
-import it.asansonne.management.enumeration.AlchemicalMixturesType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import java.util.List;
-import java.util.UUID;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Builder
-@Entity
-@Table(name = "mixtures")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@EqualsAndHashCode
-@ToString
-public class Mixture implements Models {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id")
-  @ToString.Exclude
-  private Integer id;
-
-  @Column(name = "uuid", nullable = false, unique = true, columnDefinition = "UUID")
-  private UUID uuid;
-
-  @Column(name = "type")
-  private AlchemicalMixturesType type;
-
-  @Column(name = "name")
-  private String name;
-
-  @Column(name = "administration")
-  private String administration;
-
-  //TODO gestire una manyToMany tra effetti e ricette
-  @Column(name = "effect")
-  private Effect effect;
-
-  //TODO gestire una oneToMany tra ingredienti e ricette
-  @Column(name = "recipes")
-  private List<Recipe> recipes;
-}
+//package it.asansonne.management.model;
+//
+//import it.asansonne.authhub.model.Models;
+//import it.asansonne.management.enumeration.AlchemicalMixturesType;
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
+//import java.util.List;
+//import java.util.UUID;
+//import lombok.AccessLevel;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.EqualsAndHashCode;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+//@Builder
+//@Entity
+//@Table(name = "mixtures")
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor(access = AccessLevel.PRIVATE)
+//@EqualsAndHashCode
+//@ToString
+//public class Mixture implements Models {
+//  @Id
+//  @GeneratedValue(strategy = GenerationType.IDENTITY)
+//  @Column(name = "id")
+//  @ToString.Exclude
+//  private Integer id;
+//
+//  @Column(name = "uuid", nullable = false, unique = true, columnDefinition = "UUID")
+//  private UUID uuid;
+//
+//  @Column(name = "type")
+//  private AlchemicalMixturesType type;
+//
+//  @Column(name = "name")
+//  private String name;
+//
+//  @Column(name = "administration")
+//  private String administration;
+//
+//  //TODO gestire una manyToMany tra effetti e ricette
+//  @Column(name = "effect")
+//  private Effect effect;
+//
+//  //TODO gestire una oneToMany tra ingredienti e ricette
+//  @Column(name = "recipes")
+//  private List<Recipe> recipes;
+//}
