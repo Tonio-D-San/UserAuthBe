350e410acfb5e6647a6d9602458ec6fdad3d78dc
Antonio Sansonne <asansonne@cybsec.it>
2025-11-29 23:29:15 +0100

    refactor pom

--- src/main/java/it/asansonne/authhub/AuthHubApplication.java
+++ src/main/java/it/asansonne/authhub/AuthHubApplication.java
@@ -8,11 +8,19 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "it.asansonne")
-@EnableJpaRepositories(basePackages = {"it.asansonne.authhub.ccsr.repository", "it.asansonne.management.ccsr.repository"})
-@EntityScan(basePackages = {"it.asansonne.authhub.model", "it.asansonne.management.model"})
+@EnableJpaRepositories(basePackages = {
+    "it.asansonne.authhub.ccsr.repository",
+    "it.asansonne.management.ccsr.repository",
+    "it.asansonne.payments.ccsr.repository"
+})
+@EntityScan(basePackages = {
+    "it.asansonne.authhub.model",
+    "it.asansonne.management.model",
+    "it.asansonne.payments.model"
+})
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class AuthHubApplication {
-  static void main(String[] args) {
+  public static void main(String[] args) {
     SpringApplication.run(AuthHubApplication.class, args);
   }
 }
